44c25faaddad172113917cb4dc63ea2e533e91cb
alfredopa <alfredopanu@gmail.com>
2026-04-10 17:39:50 +0200

    versione edge con menudrawer

--- app/src/main/java/com/apstudio/sentieri/MappaFragment.kt
+++ app/src/main/java/com/apstudio/sentieri/MappaFragment.kt
@@ -1040,54 +1040,53 @@ class MappaFragment : Fragment(), MenuProvider, SharedPreferences.OnSharedPrefer
             currentTrackPolyline.setPoints(fullTrackSnapshot)
 
             gpsMarker.setVisible(viewModel.isRecording) // Mostra solo se stiamo attivamente registrando
+        }
+        if (viewModel.isRecording) {
+            LocationRepository.updateGpsStatus(LocationRepository.gpsStatus.value!!)
+            accendiSchermo()
 
-            if (viewModel.isRecording) {
-                LocationRepository.updateGpsStatus(LocationRepository.gpsStatus.value!!)
-                accendiSchermo()
+            viewModel.locationData.value?.geoPoint?.let { gpsMarker.position = it }
+            gpsMarker.setVisible(true)
 
-                viewModel.locationData.value?.geoPoint?.let { gpsMarker.position = it }
-                gpsMarker.setVisible(true)
+            bottomSheetBehavior.isHideable = false
+            bottomSheetBehavior.peekHeight = 120
 
-                bottomSheetBehavior.isHideable = false
-                bottomSheetBehavior.peekHeight = 120
-
-                val stateToRestore = viewModel.bottomState
-                if (stateToRestore == BottomSheetBehavior.STATE_COLLAPSED ||
-                    stateToRestore == BottomSheetBehavior.STATE_EXPANDED ||
-                    stateToRestore == BottomSheetBehavior.STATE_HALF_EXPANDED
-                ) {
-                    bottomSheetBehavior.state = stateToRestore
-                    if (stateToRestore == BottomSheetBehavior.STATE_HALF_EXPANDED || stateToRestore == BottomSheetBehavior.STATE_EXPANDED) {
-                        //Log.d(TAG, "Ripristinato su HALF_EXPANDED. Ricarico i dati visivi.")
-                        // Forza l'aggiornamento di TUTTI gli elementi del cruscotto forzando
-                        // un nuovo emission sul LiveData (se possibile) o un aggiornamento manuale.
-                        val numberFormat = NumberFormat.getNumberInstance(Locale.getDefault())
-                        // Aggiornamento manuale forzato dei valori noti al ViewModel:
-                        viewModel.quota.value?.let {
-                            binding.cruscotto.tvQuota.text = numberFormat.format(it)
-                        }
-                        viewModel.dislivPiu.value?.let {
-                            binding.cruscotto.tvDPiu.text = numberFormat.format(it.toInt())
-                        }
-                        viewModel.dislivMeno.value?.let {
-                            binding.cruscotto.tvDMeno.text = numberFormat.format(it.toInt())
-                        }
-                        // Ridisegna la view per essere sicuro
-                        binding.cruscotto.root.invalidate()
+            val stateToRestore = viewModel.bottomState
+            if (stateToRestore == BottomSheetBehavior.STATE_COLLAPSED ||
+                stateToRestore == BottomSheetBehavior.STATE_EXPANDED ||
+                stateToRestore == BottomSheetBehavior.STATE_HALF_EXPANDED
+            ) {
+                bottomSheetBehavior.state = stateToRestore
+                if (stateToRestore == BottomSheetBehavior.STATE_HALF_EXPANDED || stateToRestore == BottomSheetBehavior.STATE_EXPANDED) {
+                    //Log.d(TAG, "Ripristinato su HALF_EXPANDED. Ricarico i dati visivi.")
+                    // Forza l'aggiornamento di TUTTI gli elementi del cruscotto forzando
+                    // un nuovo emission sul LiveData (se possibile) o un aggiornamento manuale.
+                    val numberFormat = NumberFormat.getNumberInstance(Locale.getDefault())
+                    // Aggiornamento manuale forzato dei valori noti al ViewModel:
+                    viewModel.quota.value?.let {
+                        binding.cruscotto.tvQuota.text = numberFormat.format(it)
                     }
-                    } else {
-                    Log.w(
-                        TAG,
-                        "Stato BottomSheet non valido ($stateToRestore). Imposto collassato di default."
-                    )
-                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+                    viewModel.dislivPiu.value?.let {
+                        binding.cruscotto.tvDPiu.text = numberFormat.format(it.toInt())
+                    }
+                    viewModel.dislivMeno.value?.let {
+                        binding.cruscotto.tvDMeno.text = numberFormat.format(it.toInt())
+                    }
+                    // Ridisegna la view per essere sicuro
+                    binding.cruscotto.root.invalidate()
                 }
-                showCustomSnackbar(binding.root, "Registrazione in corso")
             } else {
-                // Se la registrazione è finita ma c'erano punti, assicurati che il BottomSheet sia nascosto
-                bottomSheetBehavior.isHideable = true
-                bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                Log.w(
+                    TAG,
+                    "Stato BottomSheet non valido ($stateToRestore). Imposto collassato di default."
+                )
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
             }
+            showCustomSnackbar(binding.root, "Registrazione in corso")
+        } else {
+            // Se la registrazione è finita ma c'erano punti, assicurati che il BottomSheet sia nascosto
+            bottomSheetBehavior.isHideable = true
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
         }
 
         mapView.invalidate()
@@ -3027,25 +3026,38 @@ class MappaFragment : Fragment(), MenuProvider, SharedPreferences.OnSharedPrefer
         bottomSheetDialog.setContentView(view)
 
         val navigationView = view.findViewById<NavigationView>(R.id.navigation_view)
+        navigationView.itemIconTintList = null
 
+        // USA QUESTA (se non l'hai già fatto nell'XML):
+        /*navigationView.itemIconTintList = ContextCompat.getColorStateList(requireContext(), R.color.menu_selector_color)
+        navigationView.itemTextColor = ContextCompat.getColorStateList(requireContext(), R.color.menu_selector_color)
+        if (viewModel.isRecording) {
+            navigationView.menu.findItem(R.id.menu_gps).isChecked = true
+        } else {
+            // Altrimenti potresti voler evidenziare la lista o nulla
+            navigationView.menu.findItem(R.id.menu_lista).isChecked = true
+        }*/
         navigationView.setNavigationItemSelectedListener { menuItem ->
-            // Feedback visivo automatico gestito da NavigationItem
-            menuItem.isChecked = true
 
             when (menuItem.itemId) {
                 R.id.menu_lista -> {
                     val directions = MappaFragmentDirections.actionMappaFragmentToSentieriFragment()
                     findNavController().navigate(directions)
                 }
+
                 R.id.menu_gps -> {
                     if (viewModel.isRecording) stopGPS() else avviaLogicaRegistrazione()
                 }
+
                 R.id.menu_poi -> {
                     if (viewModel.isRecording && viewModel.isFixed) creaWayPoint()
-                    else Toast.makeText(requireContext(), "Waypoint non disponibili", Toast.LENGTH_SHORT).show()
+                    else Toast.makeText(
+                        requireContext(),
+                        "Waypoint non disponibili",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
-
             bottomSheetDialog.dismiss()
             true
         }
