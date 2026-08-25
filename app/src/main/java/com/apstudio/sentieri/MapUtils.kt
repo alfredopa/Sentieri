a8210b8be805a21a4638b4d93e065c43cd92e6fd
alfredopa <alfredopa@tiscali.it>
2026-08-25 16:56:28 +0200

    apre file kml

--- app/src/main/java/com/apstudio/sentieri/MapUtils.kt
+++ app/src/main/java/com/apstudio/sentieri/MapUtils.kt
@@ -1,9 +1,13 @@
 package com.apstudio.sentieri
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.icu.text.DecimalFormat
 import android.location.Location
 import android.net.Uri
@@ -649,5 +653,20 @@ object MapUtils {
             Log.e("MapUtils", "Errore durante la decompressione di $nomeZip", e)
             false // Restituisce false in caso di qualsiasi eccezione
         }
+    }
+
+    fun convertDrawableToBitmap(drawable: android.graphics.drawable.Drawable): Bitmap {
+        if (drawable is android.graphics.drawable.BitmapDrawable) {
+            return drawable.bitmap
+        }
+        val bitmap = Bitmap.createBitmap(
+            drawable.intrinsicWidth.coerceAtLeast(1),
+            drawable.intrinsicHeight.coerceAtLeast(1),
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+        return bitmap
     }
 }
